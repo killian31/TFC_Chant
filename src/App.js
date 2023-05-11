66e6a88521c88e62dc9489cafb08905ef339bf78
killian31 <80256683+killian31@users.noreply.github.com>
2023-05-11 22:58:22 +0200

    feat: popup window

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import '@fortawesome/fontawesome-svg-core/styles.css';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import { useState } from "react";
-import Popup from "./Popup";
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
+import './Popup.css'
 
 const videoList = [
   {
@@ -40,10 +42,51 @@ function App() {
               <div className="search-bar">
                 <input type="text" placeholder="Rechercher un chant..." />
                 <button><FontAwesomeIcon icon={faSearch} /></button>
-                <button onClick={handleRandomVideo}>Découvrir un chant aléatoire</button>
+                <Popup trigger={<button> Découvrir un chant aléatoire</button>} position="right center" modal>
+                  {close => (
+                  <div className="modal">
+                    <button className="close" onClick={close}>
+                      &times;
+                    </button>
+                    <div className="header"> EN DÉVELOPPEMENT, en attendant voici Bafo qui chante très mal : </div>
+                    <div className="content">
+                      <div style={{display: "inline-block"}}>
+                          <iframe 
+                              width="238" 
+                              height="423" 
+                              src="https://www.youtube.com/embed/KvjSv4e5JE8"
+                              title="Nous nous sommes le TFC mal chanté par Bafo"
+                              frameborder="0"
+                              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                              allowfullscreen>
+                          </iframe>
+                      </div>
+                      <div style={{display: "inline-block", position: 'absolute', top: '50%', left: '50%', transform: 'translateY(-50%)'}}>
+                          <p>&#xAB; Nous nous sommes le TFC</p>
+                          <p>Toulouse est notre fierté</p>
+                          <p>Et ce soir on gagnera</p>
+                          <p>Et pour vous on chantera</p>
+                          <p>Allez allez TFC</p>
+                          <p>Tous ensemble il faut chanter</p>
+                          <p>Allez Toulouuuuuseeee allez Toulouuuuuseeee, allez le TFC (x2) &#xBB;</p>
+                      </div>
+                    </div>
+                    <div className="actions">
+                      <button
+                        className="button"
+                        onClick={() => {
+                          console.log('Pop-up fermée');
+                          close();
+                        }}
+                      >
+                        Fermer la fenêtre Pop-up
+                      </button>
+                    </div>
+                  </div>
+                )}
+                </Popup>
               </div>
         </div>
-      {showPopup && <Popup videoUrl={videoUrl} lyrics={lyrics} />}
     </div>
   );
 }
